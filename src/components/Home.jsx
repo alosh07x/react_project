b8c604930b8ce2d31577e3c9afe1ee1f5bfbc3ca
mohamadfarhat-droid <52230340@students.liu.edu.lb>
2025-11-23 22:41:49 +0200

    i put a photo under the navigation bar and it take the remaining size of the page and if the photo will not appear a text there is no photo will appear

---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import img3 from "./image/mohamad4.jpg";
+
+function Home() {
+  return (
+    <div className="w-full h-full ">
+      <img 
+        src={img3}
+        alt="There is no photo"
+        
+      />
+    </div>
+  );
+}
+
+export default Home;
